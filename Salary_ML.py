1321dd0ea8697860806ea516917ba0a19ab8e630
venknat <venk.natarajan@gmail.com>
2020-12-13 16:13:32 -0800

    make into pycharm project

--- Salary_ML.py
+++ Salary_ML.py
@@ -67,7 +67,7 @@ def find_optimal_lm_mod(X, y, cutoffs, test_size = .30, random_state=42, plot=Tr
         reduce_X = X.iloc[:, np.where((X.sum() > cutoff) == True)[0]]
         num_feats.append(reduce_X.shape[1])
 
-        #split the data into train and test
+        #split the data into train and test, this is fancy what I am doing
         X_train, X_test, y_train, y_test = train_test_split(reduce_X, y, test_size = test_size, random_state=random_state)
 
         #fit the model and obtain pred response
